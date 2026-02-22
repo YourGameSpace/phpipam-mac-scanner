9c0b84c237763e81c5d930b9b21eef635f52c58b
TUBEOF <admin@tubeof.de>
2026-02-23 00:11:00 +0100

    Update sleep interval to use SCAN_COOLDOWN instead of SCAN_INTERVAL

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "[INFO] Starting ipam-mac-scanner v1.0.3 ..."
 
 while true; do
     /bin/bash /app/ipam-mac-scanner.sh
-    sleep $((SCAN_INTERVAL * 60))
+    sleep $((SCAN_COOLDOWN * 60))
 done
